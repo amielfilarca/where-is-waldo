5f161c40bfa429aac80b85249e80ef6771166be2
Amiel Filarca <amielfilarca@gmail.com>
2020-10-04 13:56:50 +0800

    Updates

---
+++ precache-manifest.421592b3b1185e99a1d062c4ffd8957b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbf2fca32b54789a93a09d8ef768c50",
+    "url": "/where-is-waldo/index.html"
+  },
+  {
+    "revision": "6443678a68f7dc5a5fe2",
+    "url": "/where-is-waldo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b55013317ece180adde",
+    "url": "/where-is-waldo/static/js/2.747844d5.chunk.js"
+  },
+  {
+    "revision": "53db32a7128edc3af4dcda73b74cdd83",
+    "url": "/where-is-waldo/static/js/2.747844d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6443678a68f7dc5a5fe2",
+    "url": "/where-is-waldo/static/js/main.497fd873.chunk.js"
+  },
+  {
+    "revision": "99428b7cc07a584148b0",
+    "url": "/where-is-waldo/static/js/runtime-main.b53af3b1.js"
+  },
+  {
+    "revision": "d89b85880ce3d74acd9c20fc394a887f",
+    "url": "/where-is-waldo/static/media/Alpha_5.d89b8588.png"
+  },
+  {
+    "revision": "e42031ccff75f8388c8118e2dcabb108",
+    "url": "/where-is-waldo/static/media/Astro.e42031cc.png"
+  },
+  {
+    "revision": "0802dc4c84135c931c6559754eff386a",
+    "url": "/where-is-waldo/static/media/Baby_Yoda.0802dc4c.jpg"
+  },
+  {
+    "revision": "5fe5e4b65fd57ba550bec49a832f84c1",
+    "url": "/where-is-waldo/static/media/Bill_and_Ted.5fe5e4b6.jpg"
+  },
+  {
+    "revision": "0fe877b2956de3880ccbf8928995055a",
+    "url": "/where-is-waldo/static/media/Owl.0fe877b2.jpg"
+  },
+  {
+    "revision": "c260966d2da402e2199840700c5fc683",
+    "url": "/where-is-waldo/static/media/raidthree.c260966d.png"
+  }
+]);
